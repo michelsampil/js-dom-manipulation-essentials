670d6a0ff133e4100f4c547428a7339d14ca848b
michel <msampil@switch.com.uy>
2023-06-13 18:29:26 -0300

    todo

---
+++ index.js
@@ -0,0 +1,26 @@
+// 0 - Bajar repo, probar el codigo en un browser de una pagina. y guardar las funciones en este archivo.
+
+// 1 - Funcion wakeUpNeo()
+// cambiar toda la pagina por la imagen wake up neo
+// url(https://i.pinimg.com/originals/9b/9c/27/9b9c275d4d4c2e6b59555a673a519893.gif)
+
+// 2 - funcion nowIBelieve()
+// cambiar el texto de una pagina
+
+// 3 - Funcion changeStyles()
+// cambiar los estilos de un elemento de la pagina
+
+// 4 - Funcion beginningToBelieve()
+// cambiar todas las imagenes de la pagina por la imagen
+// https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNjuQ8L2k6-Ve8MBqn93pG6lsTy66wCuMexHWl5RMYvLtkOmbPw9iW9CL-98aOppWfDWw&usqp=CAU
+
+// 5 - Funcion cantTouchThis()
+// Agregar un event listener a un elemento
+// y mostrar un mensaje del tipo alert
+// al hacer click "can't touch this 🔨"
+
+// 6 - Funcion secretLocation()
+// Agregar event listener a un elemento con el
+// comportamiento de que rediriga al usuario al
+// pasar el mouse hacia otra pagina
+// https://www.youtube.com/watch?v=ME4k6PtVogg
